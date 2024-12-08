6a0de3241cb23cff637271a6b4d32d313659b382
Prasanth <prasanthcena968@gmail.com>
2024-12-09 04:48:51 +0530

    home screen updated

--- cp/bc.sh
+++ cp/bc.sh
@@ -3,6 +3,24 @@
 echo "backup started"
 
 current_date=$(date +%d-%m-%Y)
+yesterday=$(date -d "yesterday" '+%d-%m-%Y')
+
+##############################################
+#           Delete yesterday bc file         #
+##############################################
+
+echo "Checking yesterday backup file exists or not"
+# backup_file_06-12-2024.tar.gz
+file="/var/www/mail.skyblue.co.in/cp/backup_file_"$yesterday".tar.gz"
+
+ if [  -e "$file" ]; then
+  echo "File exists"
+  echo "removing yester'bc file"
+  rm "$file"
+else
+  echo "File does not exist"
+  echo "$file"
+fi
 
 ##############################################
 #           backup folder validation         #
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "hello monika"
+echo "server working"
 
 ?>
